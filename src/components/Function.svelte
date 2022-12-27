c0b318d3a5eb263eb0f20b09a6f57a0b17856d9e
richard <mail@richard.yt>
2022-12-27 17:38:28 +0100

    design update

--- src/components/Function.svelte
+++ src/components/Function.svelte
@@ -4,7 +4,7 @@
     export let func: FunctionData;
 </script>
 
-<div class="p-5">
+<div class="mb-4 mr-4 p-4 bg-white bg-opacity-5 rounded-xl hover:bg-opacity-10 transition-all">
     <p><b>{func.name}</b></p>
     {#if func.description}
         <p>{func.description}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Fuse from 'fuse.js';
 	import VirtualList from '@sveltejs/svelte-virtual-list';
+	import Fa from 'svelte-fa/src/fa.svelte'
+	import { faGithub } from '@fortawesome/free-brands-svg-icons'
 	import { writable } from 'svelte/store';
 
 	import Function from '../components/Function.svelte';
@@ -14,31 +16,35 @@
     });
 </script>
 
-<h1 class="text-4xl">Signatures for {all.length} functions</h1>
-<input bind:value={$searchTermStore} placeholder="name, description or signature" class="w-full"/>
-<main class="list">
-	{#if $searchTermStore.length > 0}
-		{#await fuse.search($searchTermStore)}
-			<p>Loading...</p>
-		{:then searchResults}
-			{#if searchResults.length === 0}
-				<p>No results</p>
-			{:else}
-				<VirtualList items={searchResults.map(v => v.item)} let:item>
-					<Function func={item} />
-				</VirtualList>
-			{/if}
-		{/await}
-	{:else}
-		<VirtualList items={all} let:item>
-			<Function func={item} />
-		</VirtualList>
-	{/if}
+<div class="flex">
+	<div class="flex-1 p-5">
+		<h1 class="text-4xl">Signatures for {all.length} functions</h1>
+	</div>
+	<div class="flex justify-end items-center mx-5">
+		<a href="https://github.com/horionclient/Horion" class=""><Fa icon={faGithub} size="2x"/></a>
+	</div>
+</div>
+<main>
+	<div class="mx-5">
+		<input bind:value={$searchTermStore} placeholder="name, description or signature" class="w-full bg-white bg-opacity-5 rounded-xl p-2 px-3"/>
+	</div>
+	<div class="m-5 h-[calc(100vh-200px)]">
+		{#if $searchTermStore.length > 0}
+			{#await fuse.search($searchTermStore)}
+				<p>Loading...</p>
+			{:then searchResults}
+				{#if searchResults.length === 0}
+					<p>No results</p>
+				{:else}
+					<VirtualList items={searchResults.map(v => v.item)} let:item>
+						<Function func={item} />
+					</VirtualList>
+				{/if}
+			{/await}
+		{:else}
+			<VirtualList items={all} let:item>
+				<Function func={item} />
+			</VirtualList>
+		{/if}
+	</div>
 </main>
-
-<style>
-	.list {
-		height: calc(100vh - 90px);
-		overflow: auto;
-	}
-</style>
