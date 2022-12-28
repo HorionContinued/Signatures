3d96ed1e8fae7b8b504c9054032dfcc1f2c0ff46
richard <mail@richard.yt>
2022-12-28 23:44:06 +0100

    stupid tailwind!!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,7 +53,7 @@
 			<svg fill="none" stroke="var(--text-color)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
 	  	</span>
 	</div>
-	<div class="h-[calc(100vh-{showNotice ? 360 : 190 }px)]">
+	<div style="height: calc(100vh - {showNotice ? 330 : 195 }px);">
 		{#if $searchTermStore.length > 0}
 			{#await fuse.search($searchTermStore)}
 				<p>Loading...</p>
