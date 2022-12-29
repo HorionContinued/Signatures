f1bde658e334b8edac6d78e82b69501c81b04d4f
richard <mail@richard.yt>
2022-12-29 14:31:48 +0100

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-2xl mb-2">404 | not found</h1>
+    <button class="px-4 py-2 rounded-xl bg-white bg-opacity-5 hover:bg-opacity-10 tranistion-all" on:click={() => window.location.href = "/"}>back home</button>
+</div>
