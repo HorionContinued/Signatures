8377f43258ab7a45b053668f5fd8748ee82d4f0d
dork <62394594+dorian-K@users.noreply.github.com>
2022-12-31 15:04:58 +0100

    Use more fitting filters to show ref and virtual attributes, add conversion to type for refSize

--- src/components/Entry.svelte
+++ src/components/Entry.svelte
@@ -15,6 +15,21 @@
 
     // @ts-expect-error cba to type this
     const showFull = () => modalStore.set(bind(EntryFull, { entry }));
+
+    const sizeToType = (size: number) => {
+        switch (size){
+            case 1:
+                return "int8_t";
+            case 2:
+                return "int16_t";
+            case 4:
+                return "int32_t";
+            case 8:
+                return "int64_t";
+            default:
+                return undefined;
+        }
+    };
 </script>
 
 <div class="mb-4 mr-4 pb-2 p-4 bg-white bg-opacity-5 rounded-xl hover:bg-opacity-10 transition-all" on:click={showFull} on:keypress={showFull}>
@@ -29,15 +44,19 @@
     </div>
     {#if entry.description} <p>{entry.description}</p> {/if}
     {#if entry.signature} <div class="my-1 -ml-1 max-w-2xl"><CopyField text={entry.signature}/></div> {/if}
-    {#if entry.vOffset && entry.vOffset != -1 && entry.class}
+    {#if entry.isVirtual && entry.class && entry.vOffset}
         <div class="my-1 max-w-2xl flex items-center">
             Index: <span class="w-2"/> <LinkField name={entry.class}/>+<CopyField text={entry.vOffset.toString()}/>
         </div>
     {/if}
-    {#if entry.refOffset && entry.refSize}
+    {#if entry.type == EntryType.REFERENCE}
         <div class="my-1 max-w-2xl flex items-center">
             refOffset: <span class="w-2"/><CopyField text={entry.refOffset.toString()}/><span class="w-2"/>
-            refSize: <span class="w-2"/><CopyField text={entry.refSize.toString()}/>
+            refSize: <span class="w-2"/><CopyField text={entry.refSize.toString()}/> 
+            {#if sizeToType(entry.refSize) !== undefined}
+                <span class="w-2"/>
+                <small> or </small><span class="w-2"/><CopyField text={`sizeof(${sizeToType(entry.refSize)})`}/> 
+            {/if}
         </div>
     {/if}
 </div>
